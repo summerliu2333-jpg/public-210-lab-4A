6bb40a371e5a9e8fa2992e0933a543c0d0dd91cb
Yang Liu <summerliu2333@gmail.com>
2026-02-08 00:20:46 -0800

    Milestone 1

--- 4A.cpp
+++ 4A.cpp
@@ -3,9 +3,19 @@
 
 using namespace std;
 
+struct color {
+    int red;
+    int green;
+    int blue;
+};
 
 
 int main() {
 
+    color testColor = {255, 128, 64}; // Initialize a test color
+    cout << "--- Milestone 1: Test Structure ---" << endl;
+    cout << "Test Color: R=" << testColor.red 
+         << " G=" << testColor.green 
+         << " B=" << testColor.blue << endl;
     return 0;
 }
