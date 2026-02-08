de1fd8c421348cf25d551dbe12d2eff164f00c8a
Yang Liu <summerliu2333@gmail.com>
2026-02-08 00:28:56 -0800

    Milestone 3

--- 4A.cpp
+++ 4A.cpp
@@ -1,6 +1,8 @@
 //COMSC-210-5068, Lab 4A, Yang Liu
 #include <iostream>
 #include <vector>
+#include <cstdlib>
+#include <ctime>  
 
 using namespace std;
 
@@ -12,6 +14,8 @@ struct color {
 
 
 int main() {
+    
+    srand(time(0));
 
     color testColor = {255, 128, 64}; // Initialize a test color
     cout << "--- Milestone 1: Test Structure ---" << endl;
@@ -19,13 +23,24 @@ int main() {
          << " G=" << testColor.green 
          << " B=" << testColor.blue << endl;
 
-    
     vector<color> colorVector; 
     colorVector.push_back(testColor); 
     cout << "--- Milestone 2: Test Vector ---" << endl;
     cout << "First color in vector: R=" << colorVector[0].red 
          << " G=" << colorVector[0].green 
          << " B=" << colorVector[0].blue << endl;
+
+    
+    int n = rand() % 26 + 25; // Random number(25-50)
+    cout << "--- Milestone 3: Generate " << n << " random colors ---" << endl;
+    for (int i = 0; i < n; ++i) {
+        color temp;
+        temp.red = rand() % 256;
+        temp.green = rand() % 256;
+        temp.blue = rand() % 256;
+        colorVector.push_back(temp); 
+    }
+    cout << "Total colors in vector now: " << colorVector.size() << endl;
 
     return 0;
 }
